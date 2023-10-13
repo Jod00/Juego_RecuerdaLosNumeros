add0681d028173def71f2f4cce10aef72a82d133
jod00 <daniex910@gmail.com>
2023-10-12 23:28:34 -0500

    NF funcionalidad base de puntuacion creada, arreglar errores con clase sujeto

--- bin/proyecto_final.dart
+++ bin/proyecto_final.dart
@@ -1,14 +1,13 @@
 import 'dart:io';
-
 import 'package:proyecto_final/proyecto_final.dart' as proyecto_final;
-
 import 'sujeto.dart';
+import 'package:csv/csv.dart';
 
 const numeroOrdenDirecto = [[[2,9],[5,4],[3,9,6],[6,5,2],[5,4,1,2],[9,1,6,8],[8,2,1,9,6],[7,2,3,4,9],[5,7,3,6,4,8],
 [3,8,4,1,7,5],[2,1,8,9,4,3,7],[7,8,5,2,1,6,3],[1,8,4,3,7,5,3,6],[2,7,9,6,3,1,4,8],[7,2,6,1,9,4,8,3,5],[4,3,8,9,1,7,5,6,2],
 [6,2,5,3,1,8,9,5,4,7],[9,4,3,8,7,5,2,9,6,1]]]; 
 
-void main() {
+void main() async {
   
   print('''
   **Test Digitos de WISC V **
@@ -31,13 +30,59 @@ void main() {
   sujeto(codigo!, edadInt);
 
   print('comienza el test...');
+  Map resultadoDelTest= testDirecto();
 
-  
+  sujeto.puntuacionDirecta = resultadoDelTest['aciertos'];
+  sujeto.puntuacionDirectSpan = resultadoDelTest['span']; 
+
+  List<List<dynamic>> ListaDatos = [['Codigo','Edad','Dd','SpanDd', 
+  sujeto.codigo, sujeto.edad, sujeto.puntuacionDirecta, sujeto.puntuacionDirectSpan]];
+
+  String csv = ListToCsvConverter().convert(ListaDatos);
+
+  Directory directory = Directory('C:/Users/usuario/Desktop');
+  final path = directory.path+'/'+sujeto.codigo+'.csv';
+  File file = await File(path);
+  file.writeAsString(csv);
+
+}
+// fin de funcion Main ***
+
+
+
+Map testDirecto(){
+  int aciertos= 0;
+  int fallos = 0;
+  int span =0;
+
+  for(var fila in numeroOrdenDirecto){
+    for(var subfila in fila){
+      print(subfila);
+      print('Si ha Acertado coloca [i] de lo contrario [n]');
+      String? haAcertado = stdin.readLineSync();
+      if(haAcertado != null){
+        haAcertado = haAcertado.toLowerCase();
+        if(haAcertado == 'i'){
+          aciertos++;
+        }
+        else{
+          fallos++;
+        }
+      }
+    }
+    if(fila[0].length >2){
+      span = fila[0].length - 1;
+    }
+    if(fallos == 2){
+      break;
+    }
+  }
+return{'aciertos':aciertos,'span':span};
 }
 
 void salirDelPrograma(String? entrada){
   if (entrada == 's' || entrada == "S"){
-    print('''seguro que deseas salir? presiona s de nuevo
+    print('''seguro que deseas salir? presiona [s] de nuevo
     si no deseas salir presiona cualquier otra tecla''');
     String? respuesta = stdin.readLineSync();
     if (respuesta == 's'|| entrada == "S"){
@@ -47,7 +92,7 @@ void salirDelPrograma(String? entrada){
 }
 
 int ComprobarNumero(String? entrada){
-  if (int.tryParse(entrada!) != null){
+  if (int.tryParse(entrada!) == null){
      print('valor introducido no es reconocible');
      exit(0);
   }
